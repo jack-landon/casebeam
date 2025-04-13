1ad17cdc2304ba0cbe02b8f70673effea3af9756
Jack Landon <jack@jacklandon.com>
2025-04-13 15:07:46 +1000

    Fixed AI in notepad

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,131 +1,138 @@
-import {
-  createChat,
-  createMessage,
-  createSearchResultsBulk,
-  updateMessage,
-} from "@/lib/db/queries/insert";
 import { google } from "@ai-sdk/google";
-import { auth } from "@clerk/nextjs/server";
-import {
-  streamText,
-  convertToCoreMessages,
-  Message,
-  generateText,
-  generateObject,
-} from "ai";
-import { z } from "zod";
-import { SelectMessage } from "@/lib/db/schema";
+// import { Ratelimit } from "@upstash/ratelimit";
+// import { kv } from "@vercel/kv";
+import { streamText } from "ai";
+import { match } from "ts-pattern";
 
-export const maxDuration = 30;
+// IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
+export const runtime = "edge";
 
-const searchResultsSchema = z.array(
-  z.object({
-    title: z.string(),
-    docTitle: z.string(),
-    docSummary: z.string(),
-    relevanceSummary: z.string(),
-    url: z.string(),
-    tags: z.array(z.string()),
-    excerpts: z.array(
-      z.object({
-        title: z.string(),
-        caseName: z.string(),
-        content: z.string(),
-        url: z.string(),
-      })
-    ),
-  })
-);
-
-export async function POST(req: Request) {
-  const { id: chatId, messages } = (await req.json()) as {
-    messages: Array<Omit<Message, "id">>;
-    id?: string;
-  };
-  if (!chatId) throw new Error("Chat ID is required");
-
-  const user = await auth();
-  let botMsg: SelectMessage | null = null;
-  const messageToStore = messages[messages.length - 1];
-
-  // 1. Save the user message,
-  // 2. Create an empty related assistant message,
-  // 3. Generate search results,
-  // 4. Store the search results in the database and associate it with empty assistant message
-  // 5. Generate assistant message and update empty assistant message with the generated text,
-
-  // If user is signed in, store the chat and message in the database
-  if (user.userId) {
-    if (messages.length == 1) {
-      const { text: chatTitle } = await generateText({
-        model: google("gemini-2.0-flash-001"),
-        prompt: `Generate a short summary title for this chat. The title must be less than 10 words and be 1 sentence and do not end it with a period. This is the chat topic: ${messageToStore.content}`,
-      });
-
-      await createChat({
-        id: chatId,
-        userId: user.userId,
-        name: chatTitle,
-      });
-    }
-
-    const userMsgDb = await createMessage({
-      chatId,
-      content: messageToStore.content,
-      role: messageToStore.role,
-    });
-
-    // Empty assistant message -> update after generation
-    botMsg = await createMessage({
-      chatId,
-      content: "",
-      role: "assistant",
-      responseToMsgId: userMsgDb.id,
-    });
-  }
-
-  const { object: searchResults } = await generateObject({
-    model: google("gemini-2.0-flash-001", {
-      useSearchGrounding: true,
-    }),
-    system:
-      "You generate 5-10 relevant objects for search results based on the query.",
-    messages: convertToCoreMessages(messages),
-    schema: searchResultsSchema,
-  });
-
-  if (user.userId) {
-    await createSearchResultsBulk(
-      searchResults.map((res) => ({
-        ...res,
-        userId: user.userId,
-        tags: JSON.stringify(res.tags),
-        excerpts: JSON.stringify(res.excerpts),
-        messageId: botMsg?.id,
-        chatId,
-      }))
+export async function POST(req: Request): Promise<Response> {
+  // Check if the OPENAI_API_KEY is set, if not return 400
+  if (
+    !process.env.GOOGLE_GENERATIVE_AI_API_KEY ||
+    process.env.GOOGLE_GENERATIVE_AI_API_KEY === ""
+  ) {
+    return new Response(
+      "Missing GOOGLE_GENERATIVE_AI_API_KEY - make sure to add it to your .env file.",
+      {
+        status: 400,
+      }
     );
   }
+  // if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
+  //   const ip = req.headers.get("x-forwarded-for");
+  //   const ratelimit = new Ratelimit({
+  //     redis: kv,
+  //     limiter: Ratelimit.slidingWindow(50, "1 d"),
+  //   });
 
-  const result = streamText({
-    model: google("gemini-2.0-flash-001", {
-      useSearchGrounding: true,
-    }),
-    messages: convertToCoreMessages(messages),
-    onFinish: async (data) => {
-      // Update the empty bot message with the generated text
-      if (chatId && botMsg?.id) {
-        botMsg = await updateMessage({
-          id: botMsg.id,
-          chatId,
-          content: data.text,
-          role: "assistant",
-        });
-      }
-    },
+  //   const { success, limit, reset, remaining } = await ratelimit.limit(
+  //     `novel_ratelimit_${ip}`
+  //   );
+
+  //   if (!success) {
+  //     return new Response("You have reached your request limit for the day.", {
+  //       status: 429,
+  //       headers: {
+  //         "X-RateLimit-Limit": limit.toString(),
+  //         "X-RateLimit-Remaining": remaining.toString(),
+  //         "X-RateLimit-Reset": reset.toString(),
+  //       },
+  //     });
+  //   }
+  // }
+
+  const { prompt, option, command } = await req.json();
+  const messages = match(option)
+    .with("continue", () => [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that continues existing text based on context from prior text. " +
+          "Give more weight/priority to the later characters than the beginning ones. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ])
+    .with("improve", () => [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that improves existing text. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: `The existing text is: ${prompt}`,
+      },
+    ])
+    .with("shorter", () => [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that shortens existing text. " +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: `The existing text is: ${prompt}`,
+      },
+    ])
+    .with("longer", () => [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that lengthens existing text. " +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: `The existing text is: ${prompt}`,
+      },
+    ])
+    .with("fix", () => [
+      {
+        role: "system",
+        content:
+          "You are an AI writing assistant that fixes grammar and spelling errors in existing text. " +
+          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: `The existing text is: ${prompt}`,
+      },
+    ])
+    .with("zap", () => [
+      {
+        role: "system",
+        content:
+          "You area an AI writing assistant that generates text based on a prompt. " +
+          "You take an input from the user and a command for manipulating the text" +
+          "Use Markdown formatting when appropriate.",
+      },
+      {
+        role: "user",
+        content: `For this text: ${prompt}. You have to respect the command: ${command}`,
+      },
+    ])
+    .run();
+
+  const result = await streamText({
+    prompt: messages[messages.length - 1].content,
+    maxTokens: 4096,
+    temperature: 0.7,
+    topP: 1,
+    frequencyPenalty: 0,
+    presencePenalty: 0,
+    model: google("gemini-2.0-flash-001"),
   });
 
-  return result.toDataStreamResponse({
-    sendSources: true,
-  });
+  return result.toDataStreamResponse();
 }
